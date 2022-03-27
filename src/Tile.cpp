3075605f8ff60372cd0c64d0eb4198d1b0b66caa
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2022-03-27 17:48:51 +0200

    Documentation Tile & creation of txt folder

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,10 +1,19 @@
 #include "Tile.h"
 
-
 unsigned int Tile::getId() const{ return id;}
+
 std::string Tile::getName() const{return name;};
+
 unsigned int Tile::getPrice() const { return price;}
+
 unsigned int Tile::getSellPrice() const {return sell_price; }
+
 int Tile::getOwner() const {return owner;}
+
 void Tile::sold() {owner = -1;}
+
 void Tile::bought(unsigned int buyer_id) {owner = buyer_id;}
+
+void Tile:regressionTestTile() {
+    
+}
--- src/Tile.h
+++ src/Tile.h
@@ -1,3 +1,12 @@
+/**
+ * @file Tile.h
+ * @brief Module Tile (case)
+ *
+ * Module contenant la classe Tile, le classe Tile représente un case du plateau de jeu. 
+ * Ell est composée d'un id (le numéro de la case de 0 à 39)  d'un string (nom de la case), d'un price,
+ * d'un sell_price et d'un owner représenter par un nombre dans [0..8].
+ *  
+ */
 #ifndef _TILE
 #define _TILE
 
@@ -5,25 +14,65 @@
 
 class Tile {
 
-	protected:
+protected:
 
-		unsigned int id;
-		std::string name;
-		unsigned int price;
-		unsigned int sell_price;
-		int owner;
+	unsigned char id; /**< id char non signé, les case son numéroter de 0 à 39 */
+	std::string name; /**< nom de la case */
+	unsigned int price; /**< prix de la case */
+	unsigned int sell_price; /**< prix de revent */
+	int owner; /**< propriétaire -> -1 si elle appartient a personne / -1 < owner =< 8 si la case est acheter*/
 
-	public:
+public:
+	
+	/**
+	* @brief getId() récupère l'id de la case
+	* La fonction getId -> ne prend pas de paramètre et renvoie l'id d'une case.
+	*/
+	unsigned char getId() const;
+	
+	/**
+	* @brief getName() récupère le nom associer à la case
+	* La fonction getName -> ne prend pas de paramètre et renvoie le nom d'une case.
+	*/
+	std::string getName() const;
+	
+	/**
+	* @brief getPrice() récupère le prix associer à la case
+	* La fonction getPrice -> ne prend pas de paramètre et renvoie le prix d'une case.
+	*/
+	unsigned int getPrice() const;
+	
+	/**
+	* @brief getSellPrice() récupère le prix de vente d'une case
+	* La fonction getSellPrice -> ne prend pas de paramètre et renvoie le prix d'une case.
+	*/
+	unsigned int getSellPrice() const; 
+	
+	/**
+	* @brief getOwner() récupère la valeur du joueur de la case
+	* La fonction getOwner -> ne prend pas de paramètre et renvoie l'id du joueur qui est le propriétaire ou 
+	* -1 si la case n'est pas acheter.
+	*/
+	int getOwner() const;
+	
+	/**
+	* @brief sold() permet de vendre une case
+	* La procédure sold -> ne prend pas de paramètre .
+	*/
+	void sold();
+	
+	/**
+	* @brief bought(unsigned int) met à jour les propriétaire (owner)
+	* La procédure bought -> prend en paramètre l'id de l'acheteur et, 
+	* met à jour le propriétaire de la case.
+	*/
+	void bought(unsigned int); 
 
-		unsigned int getId() const;
-		std::string getName() const;
-		unsigned int getPrice() const;
-		unsigned int getSellPrice() const; 
-		int getOwner() const;
-		void sold();
-		void bought(unsigned int); 
-
+	/**
+	* @brief Procédure qui effectue le teste de regression du module Tile
+	* Procédure regressionTestTile -> Test si les fonctions membres du module Tile fonctionnes correctement.
+	*/
+	void regressionTestDice();
 };
-
 
 #endif
