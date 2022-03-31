102c2f8498f08af3aed4e6bab9955a242051eaa8
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2022-03-31 13:31:03 +0200

    supr le owner dans property

--- src/core/Property.h
+++ src/core/Property.h
@@ -9,10 +9,9 @@ class Property :  public Tile{
 		unsigned int build_price;
 		unsigned int * rent;
 		unsigned int rent_count;
-		int owner;
 
 	public:
-		Property(std::string name, unsigned int id, unsigned int price, unsigned int sell_price, 
+		Property(std::string name, unsigned int id, unsigned int price, unsigned int sell_price,
 		unsigned int new_build_price, unsigned int rent_0, 
 		unsigned int rent_1, unsigned int rent_2, unsigned int rent_3,
 		unsigned int rent_4, unsigned int rent_5);
