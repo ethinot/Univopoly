4b18d24f1eecdf7d041c7f0b007afeeaa23806e8
Enzo <enzo.ephrem@etu.univ-lyon1.fr>
2022-04-12 08:16:38 +0200

    ignore qt stuff

--- src/qt/main_qt.cpp
+++ src/qt/main_qt.cpp
@@ -1,35 +1,39 @@
-/*
-# Installer Qt5
-# Ajouter dans le path le chemin vers bin et vers tools/mingw/bin (voir le chemin exacte suivant la version)
-# ouvrir un bash
-# qmake
-# make
-# ./release/pacman_qt
-*/
-
 #include <iostream>
-#include <QApplication>
-#include <QMainWindow>
-#include <QGraphicsPixmapItem>
-#include <QPushButton>
-#include <QtGui>
-#include <QGraphicsScene>
-#include <QGraphicsView>
-#include <QMenu>
-#include <QMenuBar>
-#include <QToolBar>
-#include <QLabel>
+
+#include <QtWidgets/QApplication>
+#include <QtGui/QImage>
+#include <QtWidgets/QLabel>
+#include <QPalette>
 
 using namespace std;
 
 
 int main(int argc, char **argv)
 {
-    
+    cout << "Hello World!" << endl;
+
     QApplication app(argc, argv);
-    QImage image
-    image.load("../../img/Univopoly_board.png");
+    QLabel label;
+
+    //QImage image("/Users/enzo/Desktop/Univopoly_Qt/univopoly/img/Univopoly_Board.png");
+
+    //Palette palette;
+    //palette.setColor(label.backgroundRole(), Qt::yellow);
+    //label.setPalette(palette);
+
+    label.setPixmap( QPixmap( "/Users/enzo/Desktop/Univopoly_Qt/univopoly/img/Univopoly_Board.png" ) );
+
+    label.setScaledContents( true );
+
+    label.setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
+
+    //label.setPixmap(QPixmap::fromImage(image));
+
+    label.show();
+
     return app.exec();
 
 }
+
+
 
--- univopoly_qt.pro
+++ univopoly_qt.pro
@@ -1,5 +1,15 @@
 TEMPLATE = app
-CONFIG += c++11 qt
-SOURCES  = src/qt/main_qt.cpp
-QT += widgets
-INCLUDEPATH +=  src/qt
+CONFIG += console c++11
+CONFIG -= app_bundle
+QT += widgets gui
+
+SOURCES += \
+        src/qt/main_qt.cpp \
+
+DISTFILES += \
+    README.md \
+    img/UCBL_logo.png \
+    img/UML_v1.png \
+    img/UML_v2.png \
+    img/Univopoly_Board.png \
+    img/Univopoly_noshadow.png
